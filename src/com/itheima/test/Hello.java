d288fc7cbd13d82461a9693181ede7e63e1cb67f
LymticsCJ <769558555@qq.com>
2017-11-08 11:35:16 +0800

    hello

---
+++ src/com/itheima/test/Hello.java
@@ -0,0 +1,13 @@
+package com.itheima.test;  
+/**  
+ * ClassName:Hello <br/>  
+ * Function:  <br/>  
+ * Date:     Nov 8, 2017 11:33:10 AM <br/>       
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("hello World");
+    }
+}   
+  
